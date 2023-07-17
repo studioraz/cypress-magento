48e52a2b733e6324c02d6d957cf70e9698a38865
denistrator <denistrator@yandex.com>
2023-07-17 19:36:57 +0300

    SRP-188: clean up code base

--- scripts/readme.js
+++ scripts/readme.js
@@ -74,6 +74,7 @@ async function getGenBlocks(content){
 
 (async () => {
 
+
     // *.use.ts test classes have a special comment -> //<gen>inbox_send ----> //</gen>
     const useCases= await glob([
         join(__dirname,'../cypress/**/*.{ts,js,json}'),
